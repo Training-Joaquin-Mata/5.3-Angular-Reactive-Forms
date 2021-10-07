34c01e272e85d8ae7616655934f95a1d5c8cd604
Joaquin Mata <joaquinmata.a@gmail.com>
2021-10-07 13:49:52 -0500

    Reactive transformations

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn} from '@angular/forms';
 
+import { debounceTime } from 'rxjs/operators';
 import { Customer } from './customer';
 
 
@@ -48,6 +49,13 @@ function emailMatcher(c:AbstractControl):{[key: string]: boolean}| null{
 export class CustomerComponent implements OnInit {
   customerForm: FormGroup;
   customer = new Customer();
+  emailMessage: string;
+
+  private validationMessages = {
+    required: "Please enter your email address",
+    email: "Please enter a valid email address ",
+  }
+
 
   constructor(private fb: FormBuilder) { }
 
@@ -73,12 +81,28 @@ export class CustomerComponent implements OnInit {
   //     sendCatalog: new FormControl(true),
   //   });
     
-  
+  this.customerForm.get('notification').valueChanges.subscribe(
+    value => this.setNotification(value)
+  )
+
+  const emailControl = this.customerForm.get('emailGroup.email');
+    emailControl.valueChanges.pipe(debounceTime(1000)).subscribe(
+      value => this.setMessage(emailControl)
+    );
+
     }
 
   save() {
     console.log(this.customerForm);
     console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+  }
+
+  setMessage(c: AbstractControl): void {
+    this.emailMessage = '';
+    if ((c.touched || c.dirty) && c.errors) {
+      this.emailMessage = Object.keys(c.errors).map(
+        key => this.validationMessages[key]).join(' ');
+    }
   }
 
   populateTestData(){
