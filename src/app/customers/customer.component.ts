16693b236f710e822893899ef82251f7aea3a70c
Joaquin Mata <joaquinmata.a@gmail.com>
2021-10-07 12:45:06 -0500

    Validators

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -1,7 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder} from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn} from '@angular/forms';
 
 import { Customer } from './customer';
+
+
+//<-------------Custom validator
+
+// function ratingRange(c: AbstractControl):{[key: string]: boolean} | null {
+//       if(c.value !== null && (isNaN(c.value)|| c.value<1 || c.value>5 )){
+//           return {'range': true}
+//       }
+//       return null;
+
+// }
+
+//---.custom validator with params
+
+function ratingRange(min: number, max:  number): ValidatorFn{
+
+  return (c: AbstractControl):{[key: string]: boolean} | null =>{
+    if(c.value !== null && (isNaN(c.value)|| c.value<min || c.value>max )){
+        return {'range': true}
+    }
+    return null;
+
+}
+}
+
+function emailMatcher(c:AbstractControl):{[key: string]: boolean}| null{
+  const emailControl= c.get('email');
+  const confirmControl= c.get('confirmEmail');
+  if(emailControl.pristine===true || confirmControl.pristine===true){
+    return null;
+  }
+  if(emailControl.value === confirmControl.value){
+    return null;
+  }
+  return {'match': true}
+}
+
 
 @Component({
   selector: 'app-customer',
@@ -17,10 +54,16 @@ export class CustomerComponent implements OnInit {
   ngOnInit() {
 
     this.customerForm = this.fb.group({
-      firstName: '',
-      lastName: '',
-      email: '',
-      sendCatalog: true
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: ['', [Validators.required, Validators.maxLength(50)]],
+      emailGroup: this.fb.group({
+        email: ['', [Validators.email, Validators.required]],
+        confirmEmail: ['', Validators.required],
+      }, {validator: emailMatcher}),
+      notification: 'email',
+      phone: '',
+      sendCatalog: true,
+      rating: [null, ratingRange(1, 5)], 
     });
 
   //   this.customerForm= new FormGroup({
@@ -51,5 +94,14 @@ export class CustomerComponent implements OnInit {
     });
   }
 
+  setNotification(notifyVia: string){
+    const phoneControl = this.customerForm.get('phone');
+    if(notifyVia==='text'){
+      phoneControl.setValidators(Validators.required);
+    }else{
+      phoneControl.clearValidators();
+    }
+    phoneControl.updateValueAndValidity();
+  }
 
 }
